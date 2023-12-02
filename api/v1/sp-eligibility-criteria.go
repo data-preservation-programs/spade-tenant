2d79a9c8323f86d5393503d872dda049c442386d
stephen-pl <139834872+stephen-pl@users.noreply.github.com>
2023-12-02 13:39:36 -0800

    Implementing SP Elibigility Criteria API

--- api/v1/sp-eligibility-criteria.go
+++ api/v1/sp-eligibility-criteria.go
@@ -1,20 +1,35 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/data-preservation-programs/spade-tenant/db"
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
-type EligibilityCriteria struct {
-	Clauses []EligibilityClause `json:"clauses"`
+type WriteEligibilityCriteria struct {
+	Clauses []WriteEligibilityClause `json:"clauses"`
 }
 
-type EligibilityClause struct {
-	Attribute string `json:"attribute"`
-	Operator  string `json:"operator"`
+type WriteEligibilityClause struct {
+	TenantID int `json:"tenant_id"`
+	CommonAttributes
+}
 
-	Value interface{} `json:"value"` // TODO: type - either []string or string
+type ReadEligibilityCriteria struct {
+	Clauses []ReadEligibilityClause `json:"clauses"`
+}
+
+type ReadEligibilityClause struct {
+	CommonAttributes
+}
+
+type CommonAttributes struct {
+	ClauseAttribute string `json:"attribute"`
+	ClauseOperator  string `json:"operator"`
+	ClauseValue     string `json:"value"` // TODO: type - either []string or string
 }
 
 // handleSetSpEligibilityCriteria godoc
@@ -26,7 +41,37 @@ type EligibilityClause struct {
 //	@Success		200	{object}	ResponseEnvelope{response=EligibilityCriteria}
 //	@Router			/sp/eligibility-criteria [post]
 func (s *apiV1) handleSetSpEligibilityCriteria(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, map[string]string{})
+
+	var eligibilityCriteria WriteEligibilityCriteria
+	err := json.NewDecoder(c.Request().Body).Decode(&eligibilityCriteria.Clauses)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelop(c, err.Error()))
+	}
+
+	err = db.DB.Transaction(func(tx *gorm.DB) error {
+		for _, eligibilityClause := range eligibilityCriteria.Clauses {
+			eligibilityClause.TenantID = GetTenantId(c)
+			res := tx.Table("tenant_sp_eligibility_clauses").Where(&eligibilityClause).Updates(&eligibilityClause)
+			if res.Error != nil {
+				return res.Error
+			}
+
+			if res.RowsAffected == 0 {
+				tx.Table("tenant_sp_eligibility_clauses").Create(&eligibilityClause)
+			} else {
+				res.Table("tenant_sp_eligibility_clauses").Save(&eligibilityClause)
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelop(c, err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, CreateSuccessResponseEnvelop(c, "Updated Eligibility clauses associated with the tenant"))
 }
 
 // handleGetSpEligibilityCriteria godoc
@@ -37,5 +82,12 @@ func (s *apiV1) handleSetSpEligibilityCriteria(c echo.Context) error {
 //	@Success		200	{object}	ResponseEnvelope{response=EligibilityCriteria}
 //	@Router			/sp/eligibility-criteria [get]
 func (s *apiV1) handleGetSpEligibilityCriteria(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, map[string]string{})
+	var eligibilityCriteria ReadEligibilityCriteria
+	res := db.DB.Table("tenant_sp_eligibility_clauses").Where("tenant_id = ? ", GetTenantId(c)).Find(&eligibilityCriteria.Clauses)
+
+	if res.Error != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelop(c, res.Error.Error()))
+	}
+
+	return c.JSON(http.StatusOK, CreateSuccessResponseEnvelop(c, eligibilityCriteria))
 }
