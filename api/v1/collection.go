817bbb6b86aa6b085309f4f2d6172215bd8b1bbb
stephen-pl <139834872+stephen-pl@users.noreply.github.com>
2024-01-05 09:21:41 -0800

    issue-54-collection (#16)
    ---
    
    <details open="true"><summary>Generated summary (powered by <a href="https://app.graphite.dev">Graphite</a>)</summary>
    
    > ## TL;DR
    > This pull request adds functionality to create, modify, and delete collections in the API. It also includes changes to the database schema to support the new collection fields.
    > 
    > ## What changed
    > - Added a new `ConfigureCollectionRouter` function to handle collection-related routes.
    > - Added a new `handleCreateCollection` function to create a new collection.
    > - Added a new `handleGetCollections` function to get information about collections.
    > - Added a new `handleModifyCollection` function to modify an existing collection.
    > - Added a new `handleDeleteCollection` function to delete a collection.
    > - Updated the `Collection` struct in the database schema to include new fields: `CollectionName`, `CollectionActive`, `CollectionPieceSource`, and `CollectionDealParams`.
    > 
    > ## How to test
    > 1. Run the application.
    > 2. Use the API endpoints `/collections` to create, modify, and delete collections.
    > 3. Verify that the changes are reflected in the database.
    > 
    > ## Why make this change
    > This change adds functionality to manage collections in the API. Collections are an important part of the system and being able to create, modify, and delete them is necessary for proper data management. The changes to the database schema allow for storing additional information about collections, such as their name, active status, piece list source, and deal parameters.
    </details>

--- api/v1/collection.go
+++ api/v1/collection.go
@@ -1,69 +1,85 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/data-preservation-programs/spade-tenant/db"
 	"github.com/google/uuid"
+	"github.com/jackc/pgtype"
 	"github.com/labstack/echo/v4"
 )
 
-type MutableCollection struct {
-	Name                   string                  `json:"name"`
-	ReplicationConstraints []ReplicationConstraint `json:"replication_constraints"`
-	PieceListSource        PieceListSource         `json:"piece_list_source"`
-	Inactive               bool                    `json:"inactive"`
-	DealParams             DealParams              `json:"deal_params"`
+type CollectionResponse struct {
+	CollectionID          uuid.UUID    `json:"collection_id"`
+	CollectionName        string       `json:"name"`
+	CollectionActive      *bool        `json:"active"`
+	CollectionPieceSource pgtype.JSONB `json:"piece_list_source"`
+	CollectionDealParams  pgtype.JSONB `json:"deal_params"`
 }
 
-type DealParams struct {
-	DurationDays     uint `json:"duration_days"`
-	StartWithinHours uint `json:"start_within_hours"`
-}
-
-type Collection struct {
-	UUID uuid.UUID `json:"uuid"`
-	MutableCollection
-}
-
-type PieceListSource struct {
-	Method            string `json:"method"`
-	PollIntervalHours int    `json:"poll_interval_hours,omitempty"`
-	ConnectionDetails string `json:"connection_details"` // TODO: better types / validation for the connection details
-}
-
-type ReplicationConstraint struct {
-	ConstraintID  int `json:"constraint_id"`
-	ConstraintMax int `json:"constraint_max"`
-}
-
-type CreateCollectionResponse struct {
-	CollectionID uint   `json:"collection_id"`
-	Status       string `json:"status"`
+func (a *apiV1) ConfigureCollectionRouter(e *echo.Group) {
+	g := e.Group("/collections")
+	g.GET("", a.handleGetCollections)
+	g.PUT("/:collectionUUID", a.handleModifyCollection)
+	g.POST("", a.handleCreateCollection)
+	g.DELETE("/:collectionUUID", a.handleDeleteCollection)
 }
 
 // handleCreateCollection godoc
 //
-//		@Summary		Creates a new collection
-//		@Security apiKey
-//	  @Param 			collection body MutableCollection true "Collection to create"
-//		@Produce		json
-//		@Success		200	{object}	ResponseEnvelope{response=Collection}
-//		@Router			/collections [post]
-func handleCreateCollection(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, map[string]string{})
-}
+//	@Summary		Creates a new collection
+//	@Security		apiKey
+//	@Param 			collection CollectionMutable body true "Collection to create"
+//	@Produce		json
+//	@Success		200	{object}	ResponseEnvelope{response=Collection}
+//	@Router			/collections [post]
+func (a *apiV1) handleCreateCollection(c echo.Context) error {
+	var collection db.Collection
 
-type GetCollectionsResponse []Collection
+	err := json.NewDecoder(c.Request().Body).Decode(&collection)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelope(c, http.StatusInternalServerError, err.Error()))
+	}
+
+	collection.TenantID = db.ID(GetTenantContext(c).TenantID)
+	collection.CollectionID = uuid.New()
+	collection.ReplicationConstraints = []db.ReplicationConstraint{{CollectionID: collection.CollectionID, ConstraintID: 0, ConstraintMax: 10}}
+	active := true
+	collection.CollectionActive = &active
+
+	err = a.db.Create(&collection).Error
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelope(c, http.StatusInternalServerError, err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, CreateSuccessResponseEnvelope(c,
+		&CollectionResponse{CollectionID: collection.CollectionID,
+			CollectionName:        collection.CollectionName,
+			CollectionActive:      collection.CollectionActive,
+			CollectionPieceSource: collection.CollectionPieceSource,
+			CollectionDealParams:  collection.CollectionDealParams}))
+}
 
 // handleGetCollections godoc
 //
 //	@Summary		Gets info about collections
-//	@Security apiKey
+//	@Security		apiKey
 //	@Produce		json
 //	@Success		200	{object}	ResponseEnvelope{response=Collection}
 //	@Router			/collections [get]
-func handleGetCollections(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, map[string]string{})
+func (a *apiV1) handleGetCollections(c echo.Context) error {
+
+	var collectionResponse []CollectionResponse
+
+	err := a.db.Model(&db.Collection{TenantID: db.ID(GetTenantContext(c).TenantID)}).Find(&collectionResponse).Error
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelope(c, http.StatusInternalServerError, err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, CreateSuccessResponseEnvelope(c, collectionResponse))
 }
 
 // handleModifyCollection godoc
@@ -75,8 +91,32 @@ func handleGetCollections(c echo.Context) error {
 //	@Produce		json
 //	@Success		200	{object}	ResponseEnvelope{response=Collection}
 //	@Router			/collections/:collectionUUID [put]
-func handleModifyCollection(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, map[string]string{})
+func (a *apiV1) handleModifyCollection(c echo.Context) error {
+	var collection CollectionResponse
+	err := json.NewDecoder(c.Request().Body).Decode(&collection)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelope(c, http.StatusInternalServerError, err.Error()))
+	}
+
+	id, err := uuid.Parse(c.Param("collectionUUID"))
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelope(c, http.StatusInternalServerError, err.Error()))
+	}
+
+	err = a.db.Model(&db.Collection{TenantID: db.ID(GetTenantContext(c).TenantID), CollectionID: id}).Updates(&collection).Error
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelope(c, http.StatusInternalServerError, err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, CreateSuccessResponseEnvelope(c,
+		&CollectionResponse{CollectionID: collection.CollectionID,
+			CollectionName:        collection.CollectionName,
+			CollectionActive:      collection.CollectionActive,
+			CollectionPieceSource: collection.CollectionPieceSource,
+			CollectionDealParams:  collection.CollectionDealParams}))
 }
 
 // handleDeleteCollection godoc
@@ -87,6 +127,12 @@ func handleModifyCollection(c echo.Context) error {
 //	@Produce		json
 //	@Success		200	{object}	ResponseEnvelope{response=bool}
 //	@Router			/collections/:collectionUUID [delete]
-func handleDeleteCollection(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, map[string]string{})
+func (a *apiV1) handleDeleteCollection(c echo.Context) error {
+	err := a.db.Delete(&db.Collection{TenantID: db.ID(GetTenantContext(c).TenantID), CollectionID: uuid.MustParse(c.Param("collectionUUID"))}).Error
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, CreateErrorResponseEnvelope(c, http.StatusInternalServerError, err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, CreateSuccessResponseEnvelope(c, "Deleted Collection associated with the tenant with id "+c.Param("collectionUUID")))
 }
